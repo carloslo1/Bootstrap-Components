164c416497847163375119ee2ec50e362225b4ef
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2023-04-23 21:48:41 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl, { delay: 5000 }) // Set the delay to null to prevent auto-hide
+    })
+    toastList.forEach(toast => toast.show()); // Show all toasts on page load
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+		target: '#navbar-example2',
+		rootMargin: '0px 0px -40%'
+	});
+});
